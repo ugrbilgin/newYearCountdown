6aa1a5f655bc31c8bf5369f16c3f76da9ac21550
Uğur Bilgin <35350117+ugurthedeveloper@users.noreply.github.com>
2020-09-12 07:23:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const newYear = 'January 1, 2021';
 
+const formatTime = (value) => (value < 10 ? '0' + value : value);
+
 function countDown() {
     const newYearsDate = new Date(newYear);
     const currentDate = new Date();
@@ -11,10 +13,10 @@ function countDown() {
     const mins = Math.floor(totalSeconds / 60) % 60 ;
     const seconds = Math.floor(totalSeconds%60);
     
-    document.getElementById('days').innerHTML = days;
-    document.getElementById('hour').innerHTML = hours;
-    document.getElementById('min').innerHTML = mins;
-    document.getElementById('sec').innerHTML = seconds;
+    document.getElementById('days').innerHTML = formatTime(days);
+    document.getElementById('hour').innerHTML = formatTime(hours);
+    document.getElementById('min').innerHTML = formatTime(mins);
+    document.getElementById('sec').innerHTML = formatTime(seconds);
     
 }
 countDown();
